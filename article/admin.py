295e4e7adf1b7c385c04e9d98a24ee3798de6911
asinajiang <2021425459@qq.com>
2018-12-01 15:45:39 +0800

    1201

--- article/admin.py
+++ article/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Article
 
 # Register your models here.
-admin.site.register(Article)
+# 可以定制后台文章管理列表第一行表头显示
+
+
+# @admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    # 表示表头显示id,author,title和created_time等
+    list_display = ("id", "author", "is_deleted", "title", "read_num", "created_time", "last_updated_time")
+    ordering = ("id", )  # 表示后台文章按照id正序排列，若设置为ordering = ("-id", )或者不设置则为倒序排列
+
+
+admin.site.register(Article, ArticleAdmin)  # 等效与上面第8行
--- article/models.py
+++ article/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User  # 引用django自带的用户模型
 
 # Create your models here.
 
@@ -6,3 +8,9 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    created_time = models.DateTimeField(default=timezone.now())
+    # created_time = models.DateTimeField(auto_now_add=True)  效果同上一行
+    last_updated_time = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=1)
+    is_deleted = models.BooleanField(default=False)
+    read_num = models.IntegerField(default=0)
